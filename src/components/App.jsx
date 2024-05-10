86bfed4ea96d1b6e371ea8398edcc3ffcf3c6314
Supernubb <painkiller_1999@mail.ru>
2024-05-10 22:33:44 +0300

    fix: форматировал текст

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,48 @@ import CoursePage from "./CoursePage";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
   const [isOpen, setOpen] = useState(false);
   const [heading, setHeading] = useState("");
-  const [target, setTarget] = useState("")
+  const [target, setTarget] = useState("");
   return (
     <div className="root">
-       <Router>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Main  isOpen={isOpen} setOpen={setOpen} heading={heading} setHeading={setHeading}  target={target} setTarget={setTarget} ></Main>} /> {/* Default component */}
-        </Route>
-        <Route path="/course" element={<CoursePage  isOpen={isOpen} setOpen={setOpen} heading={heading} setHeading={setHeading}  target={target} setTarget={setTarget} ></CoursePage>}>
-
-        </Route>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <Main
+                  isOpen={isOpen}
+                  setOpen={setOpen}
+                  heading={heading}
+                  setHeading={setHeading}
+                  target={target}
+                  setTarget={setTarget}
+                ></Main>
+              }
+            />{" "}
+            {/* Default component */}
+          </Route>
+          <Route
+            path="/course"
+            element={
+              <CoursePage
+                isOpen={isOpen}
+                setOpen={setOpen}
+                heading={heading}
+                setHeading={setHeading}
+                target={target}
+                setTarget={setTarget}
+              ></CoursePage>
+            }
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
